634310d1a9c2259d69ea268c8cf7e71d90967950
sgnu <sam.nguyen.1223@gmail.com>
2022-04-13 12:33:43 -0400

    Create calculateApproachTime function

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ function calculateAR(approachTime) {
     // AR (if under 5) = 15 - 5(time) / 600
     // AR (if above 5) = (6000 - 5t) / 750 + 5
     if (approachTime > 1200) {
-        return (15 - (5 * approachTime / 600));
+        return 15 - (5 * approachTime / 600);
     } else if (approachTime < 1200) {
-        return (((6000 - 5 * approachTime) / 750) + 5);
+        return ((6000 - 5 * approachTime) / 750) + 5;
     } else {
         return 5
+    }
+}
+
+function calculateApproachTime(approachRate) {
+    // https://osu.ppy.sh/wiki/en/Beatmapping/Approach_rate
+    if (approachRate < 5) {
+        return 1200 + 600 * (5 - approachRate) / 5;
+    } else if (approachRate > 5) {
+        return 1200 - 750 * (approachRate - 5) / 5;
+    } else {
+        return 1200;
     }
 }
